86bc26797aeba4b91532e1b54c26afb5ca5eedd5
Samuel Zimel <samuel.c.zimel@gmail.com>
2023-12-13 19:34:31 -0600

    fixes console warnings and better input hanlding

--- src/components/helpers/set-theory/input.tsx
+++ src/components/helpers/set-theory/input.tsx
@@ -1,72 +1,98 @@
-/* eslint-disable max-len */
 import React from 'react';
 import { AppContext } from '../../Context';
 import { SMButton } from '../../custom/Button';
 import { SMText } from '../../custom/Text';
+import Tippy from '@tippyjs/react';
 
+const buttonData = [
+  { text: 'A', bgColor: '#355070' },
+  { text: 'B', bgColor: '#485373' },
+  { text: 'C', bgColor: '#6d597a' },
+  { text: '∩', bgColor: '#855d79' },
+  { text: '∪', bgColor: '#9d6177' },
+  { text: '’', bgColor: '#b56576' },
+  { text: '(', bgColor: '#c56774' },
+  { text: ')', bgColor: '#d56971' },
+  { text: 'delete', bgColor: 'black' },
+];
 
 export const Input: React.FC = () => {
-  const { updateVennData } = React.useContext(AppContext);
+  const { updateVennData, tippyVisible, setTippyVisible } = React.useContext(AppContext);
   const [inputValue, setInputValue] = React.useState('Click the above buttons!');
-  // const buttonData = ['A', 'B', 'C', '∩', '∪', '’', '(', ')', 'delete'];
-  const buttonData = [
-    { text: 'A', bgColor: '#355070' },
-    { text: 'B', bgColor: '#485373' },
-    { text: 'C', bgColor: '#6d597a' },
-    { text: '∩', bgColor: '#855d79' },
-    { text: '∪', bgColor: '#9d6177' },
-    { text: '’', bgColor: '#b56576' },
-    { text: '(', bgColor: '#c56774' },
-    { text: ')', bgColor: '#d56971' },
-    { text: 'delete', bgColor: 'black' },
-  ];
+  const [error, setError] = React.useState({ errorChar: '', prevChar: '' });
 
   function handleSubmit() {
-    setInputValue('');
-    updateVennData(inputValue);
+    setTippyVisible('nonsense'); // close the tippy
+    setInputValue(''); // reset the input value
+    updateVennData(inputValue); // send the input value to the venn diagram
   }
 
-  // adds and deletes input values
   function changeInput(value: string) {
-    // change the initial text
-    if (inputValue.slice(-1) === '!') {setInputValue('');}
+    const lastChar = inputValue.trimEnd().slice(-1);
+    if (inputValue.slice(-1) === '!') {setInputValue('');} // change the initial text
+    setTippyVisible('hidden');
 
+    // handle the input value, each case checks the last inputted character
+    // and disallows certain characters after certain characters
     switch (value) {
-    case '’': {
-      const lastChar = inputValue.trimEnd().slice(-1);
-      // if the last character is a letter, add a typographic apostrophe
+    case '’': 
       if (!new Set(['∩', '∪', '(', '’']).has(lastChar)) {
-        setInputValue(prevValue => `${prevValue.trimEnd()}${value + ' '}`);
-      }
-      break;
-    }
+        return setInputValue(prevValue => `${prevValue.trimEnd()}${value + ' '}`);
+      } 
+      return handleError(lastChar, value);
     case 'delete':
-      setInputValue(prevValue => prevValue.trimEnd().slice(0, -1));
-      break;
+      return setInputValue(prevValue => prevValue.trimEnd().slice(0, -1));
     case '(':
-      setInputValue(prevValue => `${prevValue}${value}`);
-      break;
+      if (!new Set(['A', 'B', 'C', '’']).has(lastChar)) {
+        return setInputValue(prevValue => `${prevValue}${value}`);
+      } 
+      return handleError(lastChar, value);
     case ')':
-      setInputValue(prevValue => `${prevValue.trimEnd()}${value + ' '}`);
-      break;
+      if (lastChar !== '∪' && lastChar !== '∩') {
+        return setInputValue(prevValue => `${prevValue.trimEnd()}${value + ' '}`);
+      } 
+      return handleError(lastChar, value);
+    case '∪':
+    case '∩':
+      if (!new Set(['∩', '∪', '(']).has(lastChar)) {
+        return setInputValue(prevValue => `${prevValue}${value + ' '}`);
+      } 
+      return handleError(lastChar, value);
     default:
-      setInputValue(prevValue => `${prevValue}${value + ' '}`);
+      if (!new Set(['A', 'B', 'C', '’']).has(lastChar)) {
+        return setInputValue(prevValue => `${prevValue}${value + ' '}`);
+      }
+      return handleError(lastChar, value);
     }
+  }
+
+  // handles the error tooltip. Updates the error state and sets the tippy to visible
+  function handleError(prevChar: string, errorChar: string) {
+    setError({ prevChar: prevChar, errorChar: errorChar });
+    setTippyVisible('visible');
   }
 
   return (
     <>
-      <div className='inputButtonContainer' >
-        {buttonData.map((button, index) => (
-          <SMButton
-            key={index}
-            color={'white'}
-            bgColor={button.bgColor}
-            onClick={() => changeInput(button.text)}
-            text={button.text}
-          />
-        ))}
-      </div>
+      <Tippy 
+        className='tippy-tooltip'
+        placement='top-start'
+        theme='dark' 
+        arrow={false} 
+        visible={tippyVisible === 'visible'}
+        content={'You can\'t use character ' + error.errorChar + ' after ' + error.prevChar}>
+        <div className='inputButtonContainer' >
+          {buttonData.map((button, index) => (
+            <SMButton
+              key={index}
+              color={'white'}
+              bgColor={button.bgColor}
+              onClick={() => changeInput(button.text)}
+              text={button.text}
+            />
+          ))}
+        </div>
+      </Tippy>
       <div style={{ display: 'flex', flexDirection: 'row', gap: 10, marginTop: 10 }}>
         <div className='inputContainer'>
           <SMText type='default' color='white'>
