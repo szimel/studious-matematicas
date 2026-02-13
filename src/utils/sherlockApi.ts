ca0fed00f471c1b0a20bd9ae789b40a7b95ac872
Samuel Zimel <samuel.c.zimel@gmail.com>
2026-02-12 18:29:28 -0600

    remove logs

--- src/utils/sherlockApi.ts
+++ src/utils/sherlockApi.ts
@@ -1,8 +1,6 @@
-/* eslint-disable no-console */
 import { API_CONFIG } from './apiConfig';
 
 export async function getParsedData(formData: FormData) {
-  console.log(API_CONFIG.BASE_URL);
   const response = await fetch(`${API_CONFIG.BASE_URL}/analyze`, {
     method: 'POST',
     headers: {
@@ -11,9 +9,7 @@ export async function getParsedData(formData: FormData) {
     body: formData,
   });
 
-  console.log(response);
   if (!response.ok) {
-    console.log(response);
     const errorBody = await response.json().catch(() => ({}));
     throw new Error(errorBody.detail || 'Server analysis failed');
   }
